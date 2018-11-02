7f3bb21b56b12d09f5b406872c8bc8369a21bc04
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-11-01 22:53:15 -0500

    refactor: Replaced custom ToString function with STL std::to_string (#17)

--- src/helpers/helpers.h
+++ src/helpers/helpers.h
@@ -18,6 +18,9 @@ const auto WIF_SIZE = 52u;
 
 #define USE_IOT
 
+// Including missing implementations of std::to_string
+#include "stl/details/to_string.h"
+
 #include <Arduino.h>
 #include <pgmspace.h>
 
@@ -26,58 +29,29 @@ const auto WIF_SIZE = 52u;
 #undef min
 #undef max
 
-    template <typename T>
-    std::string toString(T val) {
-        return String(val).c_str();
-    };
-
-    const static inline std::string toString(uint64_t input)
-    {
-        std::string result;
-        uint8_t base = 10;
-        do {
-            char c = input % base;
-            input /= base;
-            if (c < 10) {
-                c += '0';
-            } else {
-                c += 'A' - 10;
-            };
-            result = c + result;
-        } while (input);
-        return result;
-    }
-
-#else
-
-    template <typename T>
-    std::string toString(T val) {
-        return std::to_string(val);
-    }
-
 #endif
 
-    // Write data into dst
-    template <typename T>
-    static inline void pack (std::vector<uint8_t>& dst, T& data) {
-        const uint8_t * src = reinterpret_cast<const uint8_t* >(&data);
-        dst.insert(dst.end(), src, src + sizeof (T));
-    }
+// Write data into dst
+template <typename T>
+inline void pack (std::vector<uint8_t>& dst, T& data) {
+    const uint8_t * src = reinterpret_cast<const uint8_t* >(&data);
+    dst.insert(dst.end(), src, src + sizeof (T));
+}
 
-    // Read size bytes into dst from src
-    template <typename T>
-    static inline void unpack (T* dst, uint8_t* src, size_t size = -1) {
-        memcpy(dst, src, size == -1 ? sizeof(*dst) : size);
-    }
+// Read size bytes into dst from src
+template <typename T>
+inline void unpack (T* dst, uint8_t* src, size_t size = -1) {
+    memcpy(dst, src, size == -1 ? sizeof(*dst) : size);
+}
 
 
-    // Join string vector
-    static std::string join(const std::vector<std::string>& strings) {
-        return std::accumulate(strings.begin(), strings.end(), std::string(),
-            [](const std::string& a, const std::string& b) -> std::string {
-                return a + b;
-            }
-        );
-    }
+// Join string vector
+inline std::string join(const std::vector<std::string>& strings) {
+    return std::accumulate(strings.begin(), strings.end(), std::string(),
+        [](const std::string& a, const std::string& b) -> std::string {
+            return a + b;
+        }
+    );
+}
 
 #endif
