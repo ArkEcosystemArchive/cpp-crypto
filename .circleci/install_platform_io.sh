8994a00610f0434529a805aa18f7bcbfbdffb37b
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-11-19 22:21:25 -0600

    feat: Add passphrase generation support (#25)
    * feat: Add passphrase generation support
    
    * feat:  Add support for passphrase generation
    
    Added dependency on bip39 to provide passphrase generation.  Added mnemonic class to abstract out interface.  Added tests.
    
    PIO support will be added as soon as bip39 library is approved.
    
    * fix:  Remove old files
    
    Removed old files and correct merge errors.
    
    * fix:  Remove unused code from helpers.
    
    * chore:  Update .gitignore to ignore Windows generated files
    
    * fix: Update BIP39 submodule
    
    * fix:  Disable generate_mnemonic_invalid test for release builds
    
    Disabled generate_mnemonic_invalid test for release builds since asserts are not enabled.
    
    * fix:  Fix PlatformIO
    
    * Updated BIP39 submodule commit
    * Removed ESP8266 missing symbols as they are handled in the BIP39 (not a perfect solution)
    * Added missing headers to mnemonic tests
    * Fixed up PlatformIO project files.
    
    * fix:  Add git submodule clone to circleci
    
    Added recursive git submodule clone to PlatformIO target for CircleCI.

--- .circleci/install_platform_io.sh
+++ .circleci/install_platform_io.sh
@@ -6,3 +6,6 @@ platformio update
 
 # install AUnit (2778) micro-ecc (1665) libraries
 platformio lib -g install 2778 1665
+
+# ensure all git submodules have bee clone
+git submodule update --init --recursive
---
+++ src/identities/mnemonic.cpp
@@ -0,0 +1,43 @@
+/**
+ * This file is part of Ark Cpp Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+
+#include "mnemonic.h"
+
+#include "bip39.h"
+
+#include <cassert>
+
+namespace Ark {
+namespace Crypto {
+namespace Identities {
+
+std::string Mnemonic::generate(size_t num_words /* = 12u */, Language language /* = Language::en */) {
+  static const std::map<size_t, BIP39::entropy_bits_t> words_to_entropy = {
+    { 12, BIP39::entropy_bits_t::_128 },
+    { 15, BIP39::entropy_bits_t::_160 },
+    { 18, BIP39::entropy_bits_t::_192 },
+    { 21, BIP39::entropy_bits_t::_224 },
+    { 24, BIP39::entropy_bits_t::_256 }
+  };
+  static const std::map<Language, BIP39::language> language_map = {
+    { Language::en, BIP39::language::en }
+  };
+
+  const auto entropy_it = words_to_entropy.find(num_words);
+  assert(entropy_it != words_to_entropy.end());
+
+  const auto lang = language_map.find(language);
+  assert(lang != language_map.end());
+
+  return BIP39::generate_mnemonic(entropy_it->second, lang->second).to_string();
+}
+
+}
+}
+}
