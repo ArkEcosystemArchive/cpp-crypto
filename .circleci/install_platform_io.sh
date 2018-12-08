b0cc3aad88d0900014c134e0adbb5e6c4d353d7b
Chris Johnson <chrisjohnsonmail@gmail.com>
2018-12-07 20:49:08 -0600

    refactor: Update to use bip39 from PlatformIO Library Manager (#33)
    * chore:  Update submodules
    
    * chore:  Add missing files on Windows.
    
    * fix:  Correct platformIO builds
    
    * fix:  Add bip39 to CI install script
    
    * chore:  Update docs with bip39 install for PIO

--- .circleci/install_platform_io.sh
+++ .circleci/install_platform_io.sh
@@ -4,8 +4,8 @@ sudo pip install -U platformio
 # update PlatformIO
 platformio update
 
-# install AUnit (2778) micro-ecc (1665) libraries
-platformio lib -g install 2778 1665
+# install AUnit (2778) micro-ecc (1665) bip39 (5886) libraries
+platformio lib -g install 2778 1665 5886
 
 # ensure all git submodules have bee clone
 git submodule update --init --recursive
