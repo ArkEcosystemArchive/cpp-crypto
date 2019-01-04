593f439faee1a35e7fbcf6b93c82beb31d526115
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-01-04 11:50:35 -0600

    clang-tidy: performance-unnecessary-value-param

--- src/transactions/builder.cpp
+++ src/transactions/builder.cpp
@@ -15,11 +15,11 @@ Transaction Builder::buildTransfer(std::string recipientId, uint64_t amount, std
     Transaction transaction;
     transaction.type = Enums::Types::TRANSFER;
     transaction.fee = Configuration::Fee().get(Enums::Types::TRANSFER);
-    transaction.recipientId = recipientId;
-    transaction.amount = amount;
-    transaction.vendorField = vendorField;
+    transaction.recipientId = std::move(recipientId);
+    transaction.amount = std::move(amount);
+    transaction.vendorField = std::move(vendorField);
 
-    return sign(transaction, passphrase, secondPassphrase);
+    return sign(transaction, std::move(passphrase), std::move(secondPassphrase));
 }
 
 Transaction Builder::buildSecondSignatureRegistration(std::string passphrase, std::string secondPassphrase)
@@ -31,7 +31,7 @@ Transaction Builder::buildSecondSignatureRegistration(std::string passphrase, st
     const auto publicKey = Identities::PublicKey::fromPassphrase(secondPassphrase.c_str());
     transaction.asset.signature.publicKey = publicKey.toString();
 
-    return sign(transaction, passphrase, secondPassphrase);
+    return sign(transaction, std::move(passphrase), secondPassphrase);
 }
 
 Transaction Builder::buildDelegateRegistration(std::string username, std::string passphrase, std::string secondPassphrase)
@@ -39,9 +39,9 @@ Transaction Builder::buildDelegateRegistration(std::string username, std::string
     Transaction transaction;
     transaction.type = Enums::Types::DELEGATE_REGISTRATION;
     transaction.fee = Configuration::Fee().get(Enums::Types::DELEGATE_REGISTRATION);
-    transaction.asset.delegate.username = username;
+    transaction.asset.delegate.username = std::move(username);
 
-    return sign(transaction, passphrase, secondPassphrase);
+    return sign(transaction, std::move(passphrase), std::move(secondPassphrase));
 }
 
 Transaction Builder::buildVote(std::vector<std::string> votes, std::string passphrase, std::string secondPassphrase)
@@ -49,12 +49,12 @@ Transaction Builder::buildVote(std::vector<std::string> votes, std::string passp
     Transaction transaction;
     transaction.type = Enums::Types::VOTE;
     transaction.fee = Configuration::Fee().get(Enums::Types::VOTE);
-    transaction.asset.votes = votes;
+    transaction.asset.votes = std::move(votes);
 
     const auto recipient = Identities::Address::fromPassphrase(passphrase.c_str(), Configuration::Network().get().version());
     transaction.recipientId = recipient.toString();
 
-    return sign(transaction, passphrase, secondPassphrase);
+    return sign(transaction, passphrase, std::move(secondPassphrase));
 }
 
 Transaction Builder::buildMultiSignatureRegistration(uint8_t min, uint8_t lifetime, std::vector<std::string> keysgroup, std::string passphrase, std::string secondPassphrase)
@@ -69,7 +69,7 @@ Transaction Builder::buildMultiSignatureRegistration(uint8_t min, uint8_t lifeti
     const auto recipient = Identities::Address::fromPassphrase(passphrase.c_str(), Configuration::Network().get().version());
     transaction.recipientId = recipient.toString();
 
-    return sign(transaction, passphrase, secondPassphrase);
+    return sign(transaction, passphrase, std::move(secondPassphrase));
 }
 
 Transaction Builder::sign(Transaction transaction, std::string passphrase, std::string secondPassphrase)
