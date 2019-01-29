02384ab8ac58b59004ba0b9ac4652dc989eaf13c
Simon <sleepdefic1t@users.noreply.github.com>
2019-01-29 14:23:02 -0800

    refactor: Slots
    - add ms to Network Epochs
    - refactor and fix Slot to parse Epoch ms
    - update Slots tests
    - update PIO 'temp_main.cpp' to set dummy board time

--- src/utils/slot.cpp
+++ src/utils/slot.cpp
@@ -1,44 +1,37 @@
-#include "helpers/crypto_helpers.h"  // include helpers first to pick up patched STL std::to_string functions for IoT
 
+#include "helpers/crypto_helpers.h"
 #include "utils/slot.h"
+
+#include <chrono>
+#include <sstream>
 
 #undef round
 #include "date/date.h"
 
-#include <cmath>
-#include <sstream>
-
-namespace Ark {
-namespace Crypto {
-namespace Utils {
-
-uint64_t Slot::epoch_time(uint64_t time, Crypto::Networks::AbstractNetwork network) {
-  const auto start = Slot::begin_epoch_time(network);
-  return static_cast<uint64_t>(std::floor((time - start) / 1000));
-}
-
-uint64_t Slot::begin_epoch_time(Crypto::Networks::AbstractNetwork network) {
+uint64_t Ark::Crypto::Utils::Slot::epoch(Crypto::Networks::AbstractNetwork network) {
   // https://stackoverflow.com/questions/33421450/c-c-time-zone-correct-time-conversion-to-seconds-since-epoch/33438989#33438989
   std::istringstream in(network.epoch());
-  date::sys_seconds tp;
+  std::chrono::system_clock::time_point tp;
   in >> date::parse("%FT%TZ", tp);
-  if (in.fail()) {
+  if (in.fail()) {    
     in.clear();
     in.str(network.epoch());
     in >> date::parse("%FT%T%z", tp);
-  }
-  return static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count());
+  };
+  // cast milliseconds as uint64_t in seconds(/ 1000)
+  return static_cast<uint64_t>(
+    std::chrono::duration_cast<std::chrono::milliseconds>(
+      tp.time_since_epoch()
+    ).count()
+  ) / 1000;
 }
 
-uint64_t Slot::time(uint64_t time, Crypto::Networks::AbstractNetwork network) {
-  return Slot::epoch_time(time, network);
+uint64_t Ark::Crypto::Utils::Slot::time(Crypto::Networks::AbstractNetwork network) {
+  return now() - epoch(network);
 }
 
-uint64_t Slot::real_time(uint64_t epoch_time, Crypto::Networks::AbstractNetwork network) {
-  const auto start = std::floor(Slot::begin_epoch_time(network) / 1000) * 1000;
-  return static_cast<uint64_t>(start + epoch_time * 1000.0);
+uint64_t Ark::Crypto::Utils::Slot::now() {
+  return std::chrono::duration_cast<std::chrono::seconds>(
+    std::chrono::system_clock::now().time_since_epoch()
+  ).count();
 }
-
-}  // namespace Utils
-}  // namespace Crypto
-}  // namespace Ark
