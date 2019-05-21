a4b4423b61e8c0d5cf5fa91b958ee7bcf991db17
Simon <sleepdefic1t@users.noreply.github.com>
2019-05-21 08:44:54 -0700

    refactor(slots): improve slots implementations
    - Further decouple OS code from IoT code.
    - Add missing ESP8266 functionality.
    - Allow removal of lib/STL dependency.
      - STL lib is only included to provide a conv. method for IoT to use date.h
      - date.h is now only used by OS builds, which will have the STL by default.
    - update changelog.
    - add PIO filters for lib/date.
    -

--- src/helpers/crypto_helpers.h
+++ src/helpers/crypto_helpers.h
@@ -1,4 +1,11 @@
-
+/**
+ * This file is part of Ark Cpp Crypto.
+ *
+ * (c) Ark Ecosystem <info@ark.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
 
 #ifndef CRYPTO_HELPERS_H
 #define CRYPTO_HELPERS_H
@@ -17,16 +24,8 @@ const auto WIF_SIZE = 52u;
 
 #define USE_IOT
 
-// Including missing implementations of std::to_string
-#include "stl/details/to_string.h"
-
 #include <Arduino.h>
 #include <pgmspace.h>
-
-// undef the C macros to allow the C++ STL to take over
-// This is to have compatibility with various board implementations of the STL
-#undef min
-#undef max
 
 #endif
 
--- src/utils/slot.cpp
+++ src/utils/slot.cpp
@@ -2,60 +2,81 @@
 #include "helpers/crypto_helpers.h"
 #include "utils/slot.h"
 
-#include <chrono>
-#include <sstream>
+#ifndef USE_IOT // OS Builds
 
-#undef round
-#include "date/date.h"
+  #include <chrono>
+  #include <sstream>
 
-uint64_t Ark::Crypto::Utils::Slot::epoch(Crypto::Networks::AbstractNetwork network) {
-  // https://stackoverflow.com/questions/33421450/c-c-time-zone-correct-time-conversion-to-seconds-since-epoch/33438989#33438989
-  std::istringstream in(network.epoch());
-  std::chrono::system_clock::time_point tp;
-  in >> date::parse("%FT%TZ", tp);
-  if (in.fail()) {    
-    in.clear();
-    in.str(network.epoch());
-    in >> date::parse("%FT%T%z", tp);
-  };
-  // cast milliseconds as uint64_t in seconds(/ 1000)
-  return static_cast<uint64_t>(
-    std::chrono::duration_cast<std::chrono::milliseconds>(
-      tp.time_since_epoch()
-    ).count()
-  ) / 1000;
-}
+  #undef round
+  #include "date/date.h"
+
+  uint64_t Ark::Crypto::Utils::Slot::epoch(Crypto::Networks::AbstractNetwork network) {
+    // https://stackoverflow.com/questions/33421450/c-c-time-zone-correct-time-conversion-to-seconds-since-epoch/33438989#33438989
+    std::istringstream in(network.epoch());
+    std::chrono::system_clock::time_point tp;
+    in >> date::parse("%FT%TZ", tp);
+    if (in.fail()) {    
+      in.clear();
+      in.str(network.epoch());
+      in >> date::parse("%FT%T%z", tp);
+    };
+    // cast milliseconds as uint64_t in seconds(/ 1000)
+    return static_cast<uint64_t>(
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+        tp.time_since_epoch()
+      ).count()
+    ) / 1000;
+  }
+
+  uint64_t Ark::Crypto::Utils::Slot::now() {
+    return std::chrono::duration_cast<std::chrono::seconds>(
+      std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+  }
+
+#endif
 
 uint64_t Ark::Crypto::Utils::Slot::time(Crypto::Networks::AbstractNetwork network) {
   const auto time = now() - epoch(network);
   return (time > 0) ? time : 0;
 }
 
-#ifdef ESP32
+#if defined(ESP32) || defined(ESP8266)
 
-uint64_t Ark::Crypto::Utils::Slot::now() {
-  struct tm t;
-  std::stringstream ss;
-  if(getLocalTime(&t)){
-    ss << std::mktime(&t);
-  } else {
-    return 0;
-  };
-  uint64_t temp;
-  ss >> temp;
-  return temp;
-};
+  #include <time.h>
+  #include <stdlib.h> /* strtol */
+  #include <string>
 
-#elif defined(ESP8266)  //  NOT STABLE/NOT SUPPORTED
+  uint64_t Ark::Crypto::Utils::Slot::epoch(Crypto::Networks::AbstractNetwork network) {
+    constexpr const size_t expectedLength = sizeof("2017-03-21T13:00:00.000Z") - 1;
+    if (expectedLength != 24) { return 0; } //  Unexpected ISO 8601 date/time length
 
-uint64_t Ark::Crypto::Utils::Slot::now() { return 0; }
+    std::string input(network.epoch());
 
-#else // OS Builds
+    if (input.length() < expectedLength) {
+      return 0;
+    };
 
-uint64_t Ark::Crypto::Utils::Slot::now() {
-  return std::chrono::duration_cast<std::chrono::seconds>(
-    std::chrono::system_clock::now().time_since_epoch()
-  ).count();
-}
+    struct tm time;
+    time.tm_year = strtol(&input[0], nullptr, 10) - 1900;
+    time.tm_mon = strtol(&input[5], nullptr, 10) - 1;
+    time.tm_mday = strtol(&input[8], nullptr, 10);
+    time.tm_hour = strtol(&input[11], nullptr, 10);
+    time.tm_min = strtol(&input[14], nullptr, 10);
+    time.tm_sec = strtol(&input[17], nullptr, 10);
+    time.tm_isdst = 0;
 
-#endif  // #ifndef USE_IOT
+    return mktime(&time);
+  }
+
+  /***/
+
+  // 'time(0) will collide with Slot::time
+  // so we create the call outside the Slot namespace.
+  uint64_t NOW() { return time(0); }
+
+  uint64_t Ark::Crypto::Utils::Slot::now() {
+    return NOW();
+  }
+
+#endif
