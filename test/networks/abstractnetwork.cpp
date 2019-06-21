2f1951b200c8a5fa7e8016229592521a0a70bb76
Simon <sleepdefic1t@users.noreply.github.com>
2019-06-20 18:45:11 -0700

    test(networks): fix abstractnetwork tests
    Current tests are ran in different orders per platform.
    This led to a modified global test custom_network being modified improperly.
    
    This PR properly handles that order variation by creating a local object to be modified.
    
    In future iterations, an AbstractNetwork will allow `const`.

--- test/networks/abstractnetwork.cpp
+++ test/networks/abstractnetwork.cpp
@@ -59,14 +59,16 @@ TEST(networks, abstract_network_base58_prefix_get) {
 /**/
 
 TEST(networks, abstract_network_base58_prefix_set) {
-  CUSTOM_NETWORK.setBase58Prefix(BASE58_ADDRESS_P2PKH, 0x10);
-  ASSERT_EQ(0x10, CUSTOM_NETWORK.getBase58Prefix(BASE58_ADDRESS_P2PKH));
+  AbstractNetwork testNetwork = CUSTOM_NETWORK;
 
-  CUSTOM_NETWORK.setBase58Prefix(BASE58_ADDRESS_P2SH, 0x20);
-  ASSERT_EQ(0x20, CUSTOM_NETWORK.getBase58Prefix(BASE58_ADDRESS_P2SH));
+  testNetwork.setBase58Prefix(BASE58_ADDRESS_P2PKH, 0x10);
+  ASSERT_EQ(0x10, testNetwork.getBase58Prefix(BASE58_ADDRESS_P2PKH));
 
-  CUSTOM_NETWORK.setBase58Prefix(BASE58_WIF, 0x30);
-  ASSERT_EQ(0x30, CUSTOM_NETWORK.getBase58Prefix(BASE58_WIF));
+  testNetwork.setBase58Prefix(BASE58_ADDRESS_P2SH, 0x20);
+  ASSERT_EQ(0x20, testNetwork.getBase58Prefix(BASE58_ADDRESS_P2SH));
+
+  testNetwork.setBase58Prefix(BASE58_WIF, 0x30);
+  ASSERT_EQ(0x30, testNetwork.getBase58Prefix(BASE58_WIF));
 }
 
 /**/
@@ -79,11 +81,13 @@ TEST(networks, abstract_network_bip32_prefix_get) {
 /**/
 
 TEST(networks, abstract_network_bip32_prefix_set) {
-  CUSTOM_NETWORK.setBIP32Prefix(BIP32_PREFIX_XPUB, 1000000);
-  ASSERT_EQ(1000000, CUSTOM_NETWORK.getBIP32Prefix(BIP32_PREFIX_XPUB));
+  AbstractNetwork testNetwork = CUSTOM_NETWORK;
 
-  CUSTOM_NETWORK.setBIP32Prefix(BIP32_PREFIX_XPRV, 1000001);
-  ASSERT_EQ(1000001, CUSTOM_NETWORK.getBIP32Prefix(BIP32_PREFIX_XPRV));
+  testNetwork.setBIP32Prefix(BIP32_PREFIX_XPUB, 1000000);
+  ASSERT_EQ(1000000, testNetwork.getBIP32Prefix(BIP32_PREFIX_XPUB));
+
+  testNetwork.setBIP32Prefix(BIP32_PREFIX_XPRV, 1000001);
+  ASSERT_EQ(1000001, testNetwork.getBIP32Prefix(BIP32_PREFIX_XPRV));
 }
 
 /**/
