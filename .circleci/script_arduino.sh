79d105708aa34d7cca186bcad41b6221cad1f266
Simon <sleepdefic1t@users.noreply.github.com>
2019-07-01 08:32:49 -0700

    ci: update sketch path

--- .circleci/script_arduino.sh
+++ .circleci/script_arduino.sh
@@ -8,4 +8,4 @@ mv ~/project/* ~/Arduino/libraries/cpp-crypto
 arduino-cli lib install "ArduinoJson@6.10.0"
 arduino-cli lib install "BIP66"
 
-arduino-cli compile --output temp.bin -b esp32:esp32:esp32 ~/Arduino/libraries/cpp-crypto/examples/arduino/ESP32/ESP32.ino --debug
+arduino-cli compile --output temp.bin -b esp32:esp32:esp32 ~/Arduino/libraries/cpp-crypto/examples/ESP32/ESP32.ino --debug
