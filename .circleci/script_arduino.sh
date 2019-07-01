467eab2533af301d4b726d52916c39583c867ba3
Simon <sleepdefic1t@users.noreply.github.com>
2019-07-01 08:41:32 -0700

    ci: add micro-ecc arduino dependency

--- .circleci/script_arduino.sh
+++ .circleci/script_arduino.sh
@@ -5,5 +5,6 @@ mv ~/project/* ~/Arduino/libraries/cpp-crypto
 
 arduino-cli lib install "ArduinoJson@6.10.0"
 arduino-cli lib install "BIP66"
+arduino-cli lib install "micro-ecc"
 
 arduino-cli compile --output temp.bin -b esp32:esp32:esp32 ~/Arduino/libraries/cpp-crypto/examples/ESP32/ESP32.ino --debug
