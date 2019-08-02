954881fc534949d2b0793cd8bdecb6857e80e7d5
Simon <sleepdefic1t@users.noreply.github.com>
2019-08-01 18:09:27 -0700

    Update network.cpp
    add invalid-versioned network test

--- test/common/network.cpp
+++ test/common/network.cpp
@@ -6,8 +6,15 @@
 namespace {
 const Network CustomNetwork {
   "16c891512149d6d3ff1b70e65900936140bf853a4ae79b5515157981dcc706df",
-  1, 0x53, 0xba,
+  1, 0x53, 0xaa,
   "2019-04-12T13:00:00.000Z"
+};
+const Network InvalidNetwork {
+    "2a44f340d76ffc3df204c5f38cd355b7496c9065a1ade2ef92071436bd72e867",
+    1,
+    0xaa,
+    0x00,
+    "2017-03-21T13:00:00.000Z"
 };
 }  // namespace
 
@@ -40,5 +47,8 @@ TEST(common, network_comparison_equals) {
 
 TEST(common, network_comparison_not_equal) {
   Network network(CustomNetwork);
+  ASSERT_TRUE(network != Devnet);
+
+  Network network(InvalidNetwork);
   ASSERT_TRUE(network != Devnet);
 }
