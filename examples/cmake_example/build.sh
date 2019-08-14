9184756d6b9465393c6cfbc2974d54149cf44d94
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-08-13 22:56:34 -0500

    build: Add support for out of source build (#126)

---
+++ examples/cmake_example/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -dfr build
+mkdir build
+cd build
+
+# Mimic submodule process by copying cpp-crypto src into the expected directory
+# git doesn't like nested .gitmodule files
+rm -dfr ../lib/cpp-crypto/
+mkdir ../lib/cpp-crypto/
+cp -R ../../../src ../lib/cpp-crypto/
+
+cmake ..
+cmake --build .
