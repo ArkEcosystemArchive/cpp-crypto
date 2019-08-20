596e01ba49dc474c5225abd5fe9b25fb4a086bd2
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-08-20 10:29:43 -0500

    fix: Remove explicit Arduino String disable
    per sleepdefic1t

--- src/utils/json.h
+++ src/utils/json.h
@@ -5,7 +5,6 @@
 // ArduinoJson Presets
 #define ARDUINOJSON_USE_LONG_LONG 1
 #define ARDUINOJSON_ENABLE_STD_STRING 1 // Enable 'std::string'
-#define ARDUINOJSON_ENABLE_ARDUINO_STRING 0 // disable 'String'
 
 // ArduinoJson Header
 #include "ArduinoJson.h"
