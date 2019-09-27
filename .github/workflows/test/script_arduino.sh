05f610b0d46096975a662821c584e8ac2cf895e8
Simon <sleepdefic1t@users.noreply.github.com>
2019-09-26 18:13:24 -0700

    ci: update arduino triggers

--- .github/workflows/test/script_arduino.sh
+++ .github/workflows/test/script_arduino.sh
@@ -3,7 +3,7 @@
 bash ./extras/ARDUINO_IDE.sh --auto
 
 mkdir -p ~/Arduino/libraries/cpp-crypto/
-mv ~/project/* ~/Arduino/libraries/cpp-crypto
+mv ~/work/cpp-crypto/cpp-crypto/* ~/Arduino/libraries/cpp-crypto
 
 arduino-cli lib install "ArduinoJson@6.10.0"
 arduino-cli lib install "BIP66"
