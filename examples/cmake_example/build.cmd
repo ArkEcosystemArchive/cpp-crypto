17f3aae6c43c3c94c5f45458f3c37c95b8f51c9f
Simon <sleepdefic1t@users.noreply.github.com>
2019-09-30 06:10:28 -0700

    updates paths and versions

--- examples/cmake_example/build.cmd
+++ examples/cmake_example/build.cmd
@@ -7,6 +7,8 @@ REM git doesn't like nested .gitmodule files
 rmdir /S /Q ..\lib\cpp-crypto\
 mkdir ..\lib\cpp-crypto\
 xcopy /E /Y /H /R ..\..\..\src ..\lib\cpp-crypto\
+mkdir ..\lib\extern\
+xcopy /E /Y /H /R ..\..\..\extern ..\lib\extern\
 
 IF "%var%"=="BUILD_TESTS" (
   cmake .. -DBUILD_TESTS=ON
