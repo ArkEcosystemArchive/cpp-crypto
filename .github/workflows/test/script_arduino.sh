6258b40d3aae1ca6c57e6424e57d624342df183f
Chris Johnson <chrisjohnsonmail@gmail.com>
2019-11-14 09:42:50 -0600

    chore: bump avr_stl version

--- .github/workflows/test/script_arduino.sh
+++ .github/workflows/test/script_arduino.sh
@@ -8,7 +8,7 @@ mv ${GITHUB_WORKSPACE}/* ~/Arduino/libraries/cpp-crypto/
 arduino-cli lib install "ArduinoJson@6.12.0"
 arduino-cli lib install "BIP66@0.2.0"
 arduino-cli lib install "micro-ecc@1.0.0"
-arduino-cli lib install "avr_stl@1.2.1"
+arduino-cli lib install "avr_stl@1.2.2"
 
 arduino-cli compile --output temp.bin -b esp32:esp32:esp32 ~/Arduino/libraries/cpp-crypto/examples/arduino/ESP32/ESP32.ino --debug
 
