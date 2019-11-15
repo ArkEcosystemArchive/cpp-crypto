81126e601cd78fe204e86b647a3c65dc1f6e7443
Simon <sleepdefic1t@users.noreply.github.com>
2019-11-14 18:11:55 -0800

    chore(ci): update platform_io script
    - add shebang.
    - no need to `cd`.
    - upstream code-owner GH Action nudge.

--- .github/workflows/test/script_platform_io.sh
+++ .github/workflows/test/script_platform_io.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 # run PlatformIO builds
 platformio run
-cd test
-platformio run -e esp8266_tests
-platformio run -e esp32_tests
+
+platformio run -e esp8266_tests -d test
+platformio run -e esp32_tests -d test
