e25f3852739081e5f3e414a8231c08dbd28969b5
Simon <sleepdefic1t@users.noreply.github.com>
2019-12-05 09:21:56 -0800

    chore: cleanup external.cmake file

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -3,26 +3,26 @@
 # ArduinoJson: https://github.com/bblanchon/ArduinoJson
 # ------------------------------------------------------------------------------
 
-# Set the configuration
+# Copy the configuration
 configure_file(${CMAKE_SOURCE_DIR}/cmake/extern/ArduinoJson.txt.in
                ${EXTERNAL_LIBRARY_DIR}/arduinojson/CMakeLists.txt)
 
-# Execute Git Clone and run Cmake
+# Configure the Cmake build
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY ${EXTERNAL_LIBRARY_DIR}/arduinojson)
 
 if(result)
-    message(FATAL_ERROR "ArduinoJson: CMake Failed: ${result}")
+    message(FATAL_ERROR "ArduinoJson: CMake Configuration Failed: ${result}")
 endif()
 
-# Build the Cloned Repo
+# Execute Git Clone and run Cmake
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY ${EXTERNAL_LIBRARY_DIR}/arduinojson)
 
 if(result)
-    message(FATAL_ERROR "ArduinoJson: Build Failed: ${result}")
+    message(FATAL_ERROR "ArduinoJson: Clone and Build Step Failed: ${result}")
 endif()
 
 set(ARDUINO_JSON_SOURCE_DIR
@@ -37,37 +37,37 @@ include_directories(${ARDUINO_JSON_SOURCE_DIR})
 # BIP66: https://github.com/sleepdefic1t/BIP66
 # ------------------------------------------------------------------------------
 
-# Set the configuration
+# Copy the configuration
 configure_file(${CMAKE_SOURCE_DIR}/cmake/extern/BIP66.txt.in
                ${EXTERNAL_LIBRARY_DIR}/bip66/CMakeLists.txt)
 
-# Execute Git Clone and run Cmake
+# Configure the Cmake build
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY ${EXTERNAL_LIBRARY_DIR}/bip66)
 
 if(result)
-    message(FATAL_ERROR "BIP66: CMake Failed: ${result}")
+    message(FATAL_ERROR "BIP66: CMake Configuration Failed: ${result}")
 endif()
 
-# Build the Cloned Repo
+# Execute Git Clone and run Cmake
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY ${EXTERNAL_LIBRARY_DIR}/bip66)
 
 if(result)
-    message(FATAL_ERROR "BIP66: Build Failed: ${result}")
+    message(FATAL_ERROR "BIP66: Clone and Build Step Failed: ${result}")
 endif()
 
 add_subdirectory("${EXTERNAL_LIBRARY_DIR}/bip66/src/src"
                  "${EXTERNAL_LIBRARY_DIR}/bip66/build"
                  EXCLUDE_FROM_ALL)
-                 
+
 set(BIP66_SOURCE
     ${EXTERNAL_LIBRARY_DIR}/bip66/src/src/bip66.cpp
     CACHE INTERNAL "BIP66: Source Files")
 
-include_directories(${BIP66_SOURCE_DIR})
+include_directories(${EXTERNAL_LIBRARY_DIR}/bip66/src/)
 
 # ------------------------------------------------------------------------------
 
@@ -75,26 +75,26 @@ include_directories(${BIP66_SOURCE_DIR})
 # uECC: https://github.com/kmackay/micro-ecc
 # ------------------------------------------------------------------------------
 
-# Set the configuration
+# Copy the configuration
 configure_file(${CMAKE_SOURCE_DIR}/cmake/extern/uECC.txt.in
                ${EXTERNAL_LIBRARY_DIR}/uecc/CMakeLists.txt)
 
-# Execute Git Clone and run Cmake
+# Configure the Cmake build
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY ${EXTERNAL_LIBRARY_DIR}/uecc)
 
 if(result)
-    message(FATAL_ERROR "uECC: CMake Failed: ${result}")
+    message(FATAL_ERROR "uECC: CMake Configuration Failed: ${result}")
 endif()
 
-# Build the Cloned Repo
+# Execute Git Clone and run Cmake
 execute_process(COMMAND ${CMAKE_COMMAND} --build .
                 RESULT_VARIABLE result
                 WORKING_DIRECTORY ${EXTERNAL_LIBRARY_DIR}/uecc)
 
 if(result)
-    message(FATAL_ERROR "uECC: Build Failed: ${result}")
+    message(FATAL_ERROR "uECC: Clone and Build Step Failed: ${result}")
 endif()
 
 set(UECC_SOURCE_DIR
